c565eff85c973b4715fded96201c40a4294a7b52
Visweshwar Ram Marthi <vrmarthi@cita.utoronto.ca>
2018-09-26 17:56:48 -0400

    Update pipeline.py with changes for gated dynamic spectrum, estimating pulse phase sweep and gating without intervention

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,8 @@
 import numpy as np
 import astropy.io.fits as f
 import matplotlib.pylab as pl
+import glob as glob
+import numpy.fft as ft
 
 def waterfall(dds_t, P0, tsamp, sweep): 
     truebins = P0/tsamp
@@ -64,6 +66,15 @@ def get_sweep(wfall):
         else:
             continue
     sweep = np.int(np.rint(t0[0][0]) - np.rint(t1[0][0]))
+    return sweep
+
+
+def find_sweep(wfall):
+    nperiod=wfall.shape[0]
+    nbin = wfall.shape[1]
+    firstpeak = np.argmax(wfall[0,:])
+    lastpeak = np.argmax(wfall[-2,:])
+    swp = 1.0*(lastpeak-firstpeak)/(nperiod-1)
     return sweep
 
 def phase_align(dds, P0, tsamp, sweep):
@@ -156,6 +167,13 @@ def get_Period(dds_t, tsamp):
     pl.savefig('First-12-harmonics.eps')
     pl.savefig('First-12-harmonics.png')
     return P0, F0
+
+def find_gate(wfall)
+# works only for B1133+16 with tsamp=2.6ms
+    profile=wfall.mean(0)
+    peak_bin=np.where(np.max(profile))
+    g_on = [np.int(peak-6), np.int(peak+16)]
+    return g_on
 
 def select_gate(wfall):
     g_on = [0, 0]
@@ -263,6 +281,43 @@ def ds_to_fits(data, P, f_start, BW, filename):
  update_fits_header(hdu)
  hdu.writeto(filename)
 
+
+def ds_to_cs_to_fits(data, P, BW, filename):
+ ntime,nchan=data.shape
+ new_ntime=nearest_power_of_2(data.shape[0])
+ new_nchan = nearest_power_of_2(data.shape[1])
+ print(new_ntime, 'X', new_nchan, ' 2D FFT in progress...')
+ cs=ft.fftshift(ft.rfft2(data, [new_ntime, new_nchan]),axes=0)
+ print('FFT shift...')
+ realCS=np.real(cs)
+ imagCS=np.imag(cs)
+ CS=np.zeros([2,cs.shape[0], cs.shape[1]])
+ CS[0,:]=realCS
+ CS[1,:]=imagCS
+ tau = ft.rfftfreq(new_nchan,np.abs(BW)/nchan)
+ fD=ft.fftshift(ft.fftfreq(new_ntime,P))*1e3
+ hdu = f.PrimaryHDU(CS)
+ hdu.header['CTYPE1'] ='DELAY - MICROSEC'
+ hdu.header['CTYPE2'] ='DOPPLER FREQUENCY - MILLIHERTZ'
+ hdu.header['CTYPE3'] ='REAL-IMAG'
+ hdu.header['CRVAL1'] = tau[0]
+ hdu.header['CRVAL2'] = fD[0]
+ hdu.header['CRVAL3'] = 0
+ hdu.header['CDELT1'] = tau[1]-tau[0]
+ hdu.header['CDELT2'] = fD[1]-fD[0]
+ hdu.header['CDELT3'] = 0
+ hdu.header['CRPIX1'] = 0
+ hdu.header['CRPIX2'] = 0
+ hdu.header['CRPIX3'] = 0
+ hdu.header['CROTA1'] = 0.0
+ hdu.header['CROTA2'] = 0.0
+ hdu.header['CROTA3'] = 0.0
+ hdu.header['BSCALE'] = 1.0
+ update_fits_header(hdu)
+ hdu.header['INT_TIME']=P*ntime
+ hdu.writeto(filename)
+ return fD, tau, cs
+
 def make_dynamic_spectrum_cross_beam(dds, P0, tsamp, g_on):
   stride = np.int(np.rint(P0/tsamp))
   nsamples, nchan = dds.shape
@@ -278,3 +333,79 @@ def make_dynamic_spectrum_cross_beam(dds, P0, tsamp, g_on):
 
   return dynspec
 
+def stitch_dspec():                                                           
+ b3=sorted(glob.glob('B3*fits'))                                             
+ b4=sorted(glob.glob('B4*fits'))                                             
+ F0=f.open(b3[0])                                                            
+ F1=f.open(b3[-1])                                                           
+ G0=f.open(b4[0])
+ G1=f.open(b4[-1])
+ t0=F0[0].header['STRT-UTC']
+ print('First scan starts at : ',t0)
+ t1=F1[0].header['STRT-UTC']
+ print('Last scan starts at : ', t1)
+ t=np.int( ((int(t1.split(':')[0])-int(t0.split(':')[0]))*3600.0 + (int(t1.split(':')[1])-int(t0.split(':')[1]))*60.0 + (float(t1.split(':')[2])-float(t0.split(':')[2])) / 60 ) + F1[0].header['NAXIS2'] )
+ print('No. of pulse periods :',  t)
+ ds=np.zeros([t+10,16384*2+4096])
+ ds[0:G0[0].header['NAXIS2'],0:16384]=G0[0].data
+ ds[0:F0[0].header['NAXIS2'],16383+4096:-1]=F0[0].data
+ for i in range(1,len(b3)):
+   F=f.open(b3[i])
+   G=f.open(b4[i])
+   t=F[0].header['STRT-UTC']
+   offset=np.int( (int(t.split(':')[0])-int(t0.split(':')[0]))*3600.0 + (int(t.split(':')[1])-int(t0.split(':')[1]))*60.0 + (float(t.split(':')[2])-float(t0.split(':')[2])) / 60)
+   print('Scan : ',i,'starting at :', t,'with ',offset, 'pulses')
+   ds[offset:offset+G[0].data.shape[0],0:16384]=G[0].data
+   ds[offset:offset+F[0].data.shape[0],16383+4096:-1]=F[0].data
+ return ds
+  
+
+# def fit_parabola(CS, outer_a, inner_a, N, f_D, tau):
+#   a = np.linspace(outer_a, inner_a, N)
+#   XX, NX, YY, NY = 4096, 8, 256, 128
+#   cs = (np.abs(CS[:,1:])).reshape([XX, NX, YY, NY]).mean(axis=3).mean(axis=1)
+#   flux=np.zeros(N)
+#   for i in np.arange(N):
+#     fDmax = np.sqrt(tau[-1]/a[i])
+#     fdsize = np.int(1.0 * fDmax/f_D[-1] * XX)
+#     fD = np.linspace(-fDmax,fDmax, fdsize)
+#     t = a[i] * fD**2
+#     y = np.zeros(fdsize)
+#     mask = np.zeros(cs.shape)
+#     for x in np.arange(fdsize):
+# 	y = np.int(np.rint(t[x]*(YY-1))/tau[-1])
+#         mask[np.int(XX/2 - (fdsize+1)/2 + x), y ] = 1.0
+#         mask[np.int(XX/2 - (fdsize+1)/2 + x) - 1, y ] = 1.0
+#         mask[np.int(XX/2 - (fdsize+1)/2 + x) + 1, y ] = 1.0
+#         mask[np.int(XX/2 - (fdsize+1)/2 + x) - 2, y ] = 1.0
+#         mask[np.int(XX/2 - (fdsize+1)/2 + x) + 2, y ] = 1.0
+#     flux[i] = (cs*mask).sum()
+#     flux[i] = (cs*mask).sum()
+#     print(i, a[i], flux[i])
+#   return a,flux
+
+
+def make_gated_dynamic_spectrum(dds, P0, tsamp, g_on, w_cor):
+  ngate = g_on[1] - g_on[0] 
+  stride = np.int(np.rint(P0/tsamp))
+  nsamples, nchan = dds.shape
+  nbins = np.int(np.rint(P0/tsamp))
+  npulses = np.min(np.int(nsamples/nbins),w_cor.shape[0]) - 2
+  dynspec = np.zeros([ngate, npulses+1, nchan])
+
+  for i in np.arange(npulses):
+     print('Processed ', i, ' pulses', end='\r')
+     on_start = g_on[0] + stride*i
+     on_stop  = g_on[1] + stride*i
+
+     off_start_1 = stride*i
+     off_stop_1  = on_start-1
+
+     off_start_2 = on_stop+1
+     off_stop_2 = stride*(i+1)
+     
+     off_pulse = (dds[off_start_1:off_stop_1,:].mean(axis=0) + dds[off_start_2:off_stop_2,:].mean(axis=0)) * 0.5
+
+     dynspec[:,i,:] = ((dds[on_start:on_stop,:])/off_pulse - 1.0) / w_cor[i,g_on[0]:g_on[1], None]
+
+  return dynspec
